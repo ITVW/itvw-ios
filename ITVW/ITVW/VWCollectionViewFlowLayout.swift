d4720f99f5b6957455036e7e659a639cdf18ae85
Rachel Schifano <rschifa1@gmail.com>
2016-05-04 22:02:57 -0500

    Create custom flow layout class

---
+++ ITVW/ITVW/VWCollectionViewFlowLayout.swift
@@ -0,0 +1,14 @@
+//
+//  VWCollectionViewFlowLayout.swift
+//  ITVW
+//
+//  Created by Rachel Schifano on 5/4/16.
+//  Copyright © 2016 schifano. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class VWCollectionViewFlowLayout: UICollectionViewFlowLayout {
+    
+}
