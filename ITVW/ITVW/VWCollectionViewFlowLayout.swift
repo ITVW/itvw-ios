3a3d09ff59f72c15317c67be9f8f8d2146f6e775
Rachel Schifano <rschifa1@gmail.com>
2016-05-05 23:55:17 -0500

    Override targetContentOffsetForProposedContentOffset for paging

--- ITVW/ITVW/VWCollectionViewFlowLayout.swift
+++ ITVW/ITVW/VWCollectionViewFlowLayout.swift
@@ -10,5 +10,17 @@ import Foundation
 import UIKit
 
 class VWCollectionViewFlowLayout: UICollectionViewFlowLayout {
-    
+    override func targetContentOffsetForProposedContentOffset(proposedContentOffset: CGPoint, withScrollingVelocity velocity: CGPoint) -> CGPoint {
+        var offsetAdjustment = CGFloat.max
+        let horizontalOffset = proposedContentOffset.x
+        let targetRect = CGRectMake(proposedContentOffset.x, 0, self.collectionView!.bounds.size.width, self.collectionView!.bounds.size.height)
+        
+        for layoutAttributes in super.layoutAttributesForElementsInRect(targetRect)! {
+            let itemOffset = layoutAttributes.frame.origin.x
+            if (abs(itemOffset - horizontalOffset) < abs(offsetAdjustment)) {
+                offsetAdjustment = itemOffset - horizontalOffset
+            }
+        }
+        return CGPointMake(proposedContentOffset.x + offsetAdjustment, proposedContentOffset.y)
+    }
 }
