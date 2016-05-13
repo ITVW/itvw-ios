3b1a5559d47ba4ffbab3e0052f296790c6ff948b
Rachel Schifano <rschifa1@gmail.com>
2016-05-12 23:40:24 -0500

    Create beingDragged function for DraggableView

--- ITVW/ITVW/DraggableView.swift
+++ ITVW/ITVW/DraggableView.swift
@@ -23,7 +23,7 @@ let SCALE_MAX: Float = 0.93
 let ROTATION_MAX: Float = 1
 // Strength of rotation.
 // Higher = weaker rotation
-let ROTATION_STRENGTH: Float = 320
+let ROTATION_STRENGTH: Float = 320 // Also 320px is width of iPhone 4 (?)
 // Higher = stronger rotation angle (0.3925) ?
 let ROTATION_ANGLE: Float = 3.14/8
 
@@ -51,16 +51,63 @@ class DraggableView: UIView {
         
         self.setupView()
         
+        // TODO: Add information if necessary here
+        
+        // Create and hide the overlay
+        // Create new pan gesture recognizer for swiping actions
+        pangestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(DraggableView.beingDragged(_:)))
+        
+        
     }
     
+    /*
+        Sets up the view to look like a generic card, including rounded corners, shadow radius.
+    */
     func setupView() -> Void {
-        self.layer.cornerRadius = 4
-        self.layer.shadowRadius = 3
-        self.layer.shadowOpacity = 0.2
-        self.layer.shadowOffset = CGSizeMake(1,1)
+        self.layer.cornerRadius = 4 // The radius to use when drawing rounded corners for the layer's background
+        self.layer.shadowRadius = 3 // The blur radius in points used to render the layer's shadow
+        self.layer.shadowOpacity = 0.2 // The opacity of the layer's shadow
+        self.layer.shadowOffset = CGSizeMake(1,1) // The offset in points of the layer's shadow (width, height)
+        
+        self.backgroundColor = UIColor.whiteColor() // FIXME: Does not need to be in init?
     }
     
-    
+    func beingDragged(gestureRecognizer: UIPanGestureRecognizer) -> Void {
+        // Points identifying the new location of a view in the coord system of its designated superview.
+        xFromCenter = Float(gestureRecognizer.translationInView(self).x)
+        yFromCenter = Float(gestureRecognizer.translationInView(self).y)
+        
+        switch gestureRecognizer.state {
+        case UIGestureRecognizerState.Began:
+            // Save origin position of the view so we can 'snap' back to it later
+            self.originPoint = self.center
+        case UIGestureRecognizerState.Changed:
+            let rotationStrength: Float = min(xFromCenter/ROTATION_STRENGTH, ROTATION_MAX) // returns lesser of x and y
+            let rotationAngle = ROTATION_ANGLE * rotationStrength
+            // fabsf - Floating point abs function
+            let scale = max(1 - fabsf(rotationStrength)/SCALE_STRENGTH, SCALE_MAX)
+            
+            // New center, original center + current coordinate
+            self.center = CGPointMake(self.originPoint.x + CGFloat(xFromCenter), self.originPoint.y + CGFloat(yFromCenter))
+            
+            // Every time the gesture changes update the scale, rotation transform, and position
+            let transform = CGAffineTransformMakeRotation(CGFloat(rotationAngle))
+            let scaleTransform = CGAffineTransformScale(transform, CGFloat(scale), CGFloat(scale))
+            self.transform = scaleTransform
+            self.updateOverlay(CGFloat(xFromCenter))
+        case UIGestureRecognizerState.Ended:
+            // When the pan gesture ends, animate the view back to the original position and remove transformations
+            self.afterSwipeAction()
+        case UIGestureRecognizerState.Possible:
+            fallthrough
+        case UIGestureRecognizerState.Cancelled:
+            fallthrough
+        case UIGestureRecognizerState.Failed:
+            fallthrough
+        default:
+            break
+        }
+    }
 }
 
 
